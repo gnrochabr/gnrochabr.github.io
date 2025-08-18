012dfb2426afb19ff794ca47ac39f613ec6f7363
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2025-08-18 20:22:54 -0300

    Update relatorioGeral.js

--- js/relatorioGeral.js
+++ js/relatorioGeral.js
@@ -5,7 +5,7 @@
  */
 document.addEventListener('DOMContentLoaded', () => {
     // 1. CONSTANTES E CONFIGURAÇÕES
-    const API_BASE_URL = 'https://serversgch-nv.onrender.com';
+    const API_BASE_URL = 'https://serversgch-nv.onrender.com/api';
     const DOM = {
         tableHead: document.getElementById('geralTableHead'),
         tableBody: document.getElementById('geralTableBody'),
