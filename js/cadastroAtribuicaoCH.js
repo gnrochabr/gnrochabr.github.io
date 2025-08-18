013bdec5423a9fc9c389f71f06ed8327693674dc
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2025-08-18 19:54:21 -0300

    Update cadastroAtribuicaoCH.js

--- js/cadastroAtribuicaoCH.js
+++ js/cadastroAtribuicaoCH.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://serversgch-nv.onrender.com';
 
 document.addEventListener('DOMContentLoaded', () => {
     const formSelecaoHorario = document.getElementById('formSelecaoHorario');
